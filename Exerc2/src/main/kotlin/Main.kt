15fe70541bd6fdacbb327c126f47b9b9ca70349a
Pablo Santos <99838818+P4BLOll@users.noreply.github.com>
2023-03-08 14:38:25 -0300

    Add files via upload

---
+++ Exerc2/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    val x = 3
+    val y = 4
+    val mult = x*y
+    val resul = Math.pow(mult.toDouble() , 2.0)
+
+    print("O resultado da conta (3*4)^2 é de: $resul")
+}
